50f26976924204a6e34609922080707535cdd68d
John Le <johnminhle01@gmail.com>
2023-08-04 00:13:26 -0400

    Start initial main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,12 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
+    screens: {
+      sm: '600px',
+      md: '960px',
+      lg: '1280px',
+      xl: '1920px',
+    }
   },
   plugins: [],
 }
